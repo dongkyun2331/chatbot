53b32e1054080836fc2f24bb6c8885f2b125bd4a
dongkyun2331 <119479530+dongkyun2331@users.noreply.github.com>
2023-03-30 11:53:07 +0900

    a

--- src/components/ChatBot.jsx
+++ src/components/ChatBot.jsx
@@ -135,7 +135,7 @@ function ChatBot() {
       return;
     } else {
       chatbotMessage = {
-        text: "명령어 : 영어도시명 날씨, 영어도시명 이번 주 / 한국 주요도시는 한글지원",
+        text: "명령어 : 영어도시명 날씨, 영어도시명 이번 주 (한국 주요도시는 한글지원)",
         isSent: false,
       };
     }
